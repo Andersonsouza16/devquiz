d26262d72b2106409ba10fca273b2c9c27f8eda8
Andersonsouza16 <dheell@gmail.com>
2021-04-19 22:37:26 -0300

    inclusao dos arquivos fornecidos pelo instrutor e criaçao das telas Splash e Home

---
+++ lib/home/widgets/app_bar_widget.dart
@@ -0,0 +1,26 @@
+import 'package:devquiz/core/app_gradients.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+            preferredSize: Size.fromHeight(250),
+            child: Container(
+              height: 250,
+              decoration: BoxDecoration(gradient: AppGradients.linear),
+              child: Row(
+                children: [
+                  Text('Ola Teste'),
+                  Container(
+                    width: 58,
+                    height: 58,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://avatars.githubusercontent.com/u..."))),
+                  )
+                ],
+              ),
+            ));
+}
