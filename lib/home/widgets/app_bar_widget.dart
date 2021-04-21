8e3ad9522d50f503e5578f1e079d4e610d17ee78
Andersonsouza16 <dheell@gmail.com>
2021-04-21 11:01:51 -0300

    criacao da AppBar

--- lib/home/widgets/app_bar_widget.dart
+++ lib/home/widgets/app_bar_widget.dart
@@ -1,4 +1,6 @@
 import 'package:devquiz/core/app_gradients.dart';
+import 'package:devquiz/core/app_text_styles.dart';
+import 'package:devquiz/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
@@ -7,19 +9,39 @@ class AppBarWidget extends PreferredSize {
             preferredSize: Size.fromHeight(250),
             child: Container(
               height: 250,
-              decoration: BoxDecoration(gradient: AppGradients.linear),
-              child: Row(
+              child: Stack(
                 children: [
-                  Text('Ola Teste'),
                   Container(
-                    width: 58,
-                    height: 58,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                            image: NetworkImage(
-                                "https://avatars.githubusercontent.com/u..."))),
-                  )
+                    height: 161,
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    width: double.maxFinite,
+                    decoration: BoxDecoration(gradient: AppGradients.linear),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text.rich(TextSpan(
+                            text: "Olá, ",
+                            style: AppTextStyles.title,
+                            children: [
+                              TextSpan(
+                                text: 'Nome',
+                                style: AppTextStyles.titleBold,
+                              )
+                            ])),
+                        Container(
+                          width: 58,
+                          height: 58,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://avatars.githubusercontent.com/u..."))),
+                        )
+                      ],
+                    ),
+                  ),
+                  Align(
+                      alignment: Alignment(0.0, 1.0), child: ScoreCardWidget())
                 ],
               ),
             ));
